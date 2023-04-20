f3108c36ce13e2dc06c1d441713bd4abe33ede3f
ramon <ramon@jairosoft.com>
2023-04-20 16:22:18 -0700

    added UI element

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [animalInput, setAnimalInput] = useState("I was 11, I was staying up all night, playing video games, when I heard something outside, in the bushes. I was paranoid and the blinds were wide open, so I looked outside and saw reddish, orange eyes. I could also barely make out a face, with a snout. I fell back in fear and heard a howl outside and then footsteps getting further away when I got back up. 2 minutes later, it was gone.");
+  const [systemInput, setSystemInput] = useState("I was 11, I was staying up all night, playing video games, when I heard something outside, in the bushes. I was paranoid and the blinds were wide open, so I looked outside and saw reddish, orange eyes. I could also barely make out a face, with a snout. I fell back in fear and heard a howl outside and then footsteps getting further away when I got back up. 2 minutes later, it was gone.");
+  const [userInput, setUserInput] = useState("Write a story SIMILAR to SYSTEM. writing style should be straightforward and concise. It should give the information in a clear and direct manner, without using overly descriptive language or complex sentence structures. The sentences are relatively short and simple, making it easy for the reader to understand the situation being described. This style is commonly used in journalistic writing, where clarity and brevity are essential. In first person. story should be extremely long with lots of detail for every part of the story.");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -40,16 +42,43 @@ export default function Home() {
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Horror Story Generator</h3>
         <form onSubmit={onSubmit}>
-          <input
+
+          {/* <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
+            placeholder="Enter System"
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
+          /> */}
+        
+          <textarea
+            type="textarea"
+            name="system"
+            placeholder="Enter System"
+            value={systemInput}
+            rows="13"
+            cols="20"
+            onChange={(e) => setSystemInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          
+          <textarea
+            type="textarea"
+            name="user"
+            placeholder="Enter User"
+            value={userInput}
+            rows="18"
+            cols="20"
+            onChange={(e) => setUserInput(e.target.value)}
+          />
+          
+          {/* <select onChange={(e) => setAnimalInput(e.target.value)}>
+            <option>1</option>
+            <option>2</option>
+          </select> */}
+
+          <input type="submit" value="Generate story" disabled />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
