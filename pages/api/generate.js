58fd70b54a6b4b6f6f9048b30dec2b707b4e4590
ramon <ramon@jairosoft.com>
2023-04-22 20:07:49 -0700

    added integration to chat gpt-4

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -3,9 +3,12 @@ import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
+
+  //apikey validation
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
@@ -15,23 +18,57 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  //code input validation
+  const codeInput = req.body.codeInput || '';
+  if (codeInput.trim().length === 0 || codeInput !== '20230421') {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter a valid code input",
+      }
+    });
+    return;
+  }
+
+  //system input validtion
+  const systemInput = req.body.systemInput || '';
+  if (systemInput.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid system input",
+      }
+    });
+    return;
+  }
+
+  //user input validation
+  const userInput = req.body.userInput || '';
+  if (userInput.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid user input",
       }
     });
     return;
   }
 
   try {
-    const completion = await openai.createCompletion({
+
+    /* const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+      prompt: generatePrompt(systemInput),
       temperature: 0.6,
-    });
-    res.status(200).json({ result: completion.data.choices[0].text });
+    }); */
+
+    const completion = await openai.createChatCompletion({
+      model: "gpt-4",
+      messages: [
+        {role: "system", content: JSON.stringify(systemInput)},
+        {role: "user", content: JSON.stringify(userInput)}
+      ]     
+    })
+
+    res.status(200).json({ result: JSON.stringify(completion.data.choices[0].message['content'])});
+
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
@@ -51,12 +88,13 @@ export default async function (req, res) {
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+
   return `Suggest three names for an animal that is a superhero.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+          Animal: Cat
+          Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+          Animal: Dog
+          Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+          Animal: ${capitalizedAnimal}
+          Names:`;
 }
--- pages/index.js
+++ pages/index.js
@@ -3,36 +3,43 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("I was 11, I was staying up all night, playing video games, when I heard something outside, in the bushes. I was paranoid and the blinds were wide open, so I looked outside and saw reddish, orange eyes. I could also barely make out a face, with a snout. I fell back in fear and heard a howl outside and then footsteps getting further away when I got back up. 2 minutes later, it was gone.");
-  const [systemInput, setSystemInput] = useState("I was 11, I was staying up all night, playing video games, when I heard something outside, in the bushes. I was paranoid and the blinds were wide open, so I looked outside and saw reddish, orange eyes. I could also barely make out a face, with a snout. I fell back in fear and heard a howl outside and then footsteps getting further away when I got back up. 2 minutes later, it was gone.");
-  const [userInput, setUserInput] = useState("Write a story SIMILAR to SYSTEM. writing style should be straightforward and concise. It should give the information in a clear and direct manner, without using overly descriptive language or complex sentence structures. The sentences are relatively short and simple, making it easy for the reader to understand the situation being described. This style is commonly used in journalistic writing, where clarity and brevity are essential. In first person. story should be extremely long with lots of detail for every part of the story.");
+
+  const [userInput, setUserInput] = useState("I was 11, I was staying up all night, playing video games, when I heard something outside, in the bushes. I was paranoid and the blinds were wide open, so I looked outside and saw reddish, orange eyes. I could also barely make out a face, with a snout. I fell back in fear and heard a howl outside and then footsteps getting further away when I got back up. 2 minutes later, it was gone.");
+  const [systemInput, setSystemInput] = useState("Write a story SIMILAR to SYSTEM. writing style should be straightforward and concise. It should give the information in a clear and direct manner, without using overly descriptive language or complex sentence structures. The sentences are relatively short and simple, making it easy for the reader to understand the situation being described. This style is commonly used in journalistic writing, where clarity and brevity are essential. In first person. story should be extremely long with lots of detail for every part of the story.");
+  const [codeInput, setCodeInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
     event.preventDefault();
+    
+    console.debug("codeInput=" + codeInput);
+
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ 
+          systemInput: systemInput,
+          userInput: userInput,
+          codeInput: codeInput
+        }),
       });
-
+      
       const data = await response.json();
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
-
+      
+      console.debug("data.result=" + data.result);
       setResult(data.result);
-      setAnimalInput("");
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
     }
   }
-
   return (
     <div>
       <Head>
@@ -44,21 +51,13 @@ export default function Home() {
         <img src="/dog.png" className={styles.icon} />
         <h3>Horror Story Generator</h3>
         <form onSubmit={onSubmit}>
-
-          {/* <input
-            type="text"
-            name="animal"
-            placeholder="Enter System"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          /> */}
-        
+          
           <textarea
             type="textarea"
             name="system"
             placeholder="Enter System"
             value={systemInput}
-            rows="13"
+            rows="18"
             cols="20"
             onChange={(e) => setSystemInput(e.target.value)}
           />
@@ -66,22 +65,34 @@ export default function Home() {
           <textarea
             type="textarea"
             name="user"
-            placeholder="Enter User"
+            placeholder="User Input"
             value={userInput}
-            rows="18"
+            rows="13"
             cols="20"
             onChange={(e) => setUserInput(e.target.value)}
           />
-          
-          {/* <select onChange={(e) => setAnimalInput(e.target.value)}>
-            <option>1</option>
-            <option>2</option>
-          </select> */}
-
-          <input type="submit" value="Generate story" disabled />
+          <input
+            type="text"
+            name="code"
+            placeholder="Enter Code"
+            onChange={(e) => setCodeInput(e.target.value)}
+          />
+          <input type="submit" value="Generate story" />
         </form>
-        <div className={styles.result}>{result}</div>
-      </main>
+        
+        <div className={styles.result}>
+          <form>
+            <textarea
+              type="textarea"
+              name="result"
+              placeholder=""
+              value={result}
+              rows="18"
+              cols="20"
+            />
+          </form>
+        </div>
+        </main>
     </div>
   );
 }
